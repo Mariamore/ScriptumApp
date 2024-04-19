8cfb801ee1685b4639b158959611578c423de744
maria.moreno <maria.moreno.r96@gmail.com>
2024-04-19 21:44:06 +0200

    "Agregada funcionalidad a Activity Login"

--- app/src/main/java/com/example/scriptumapp/Login.java
+++ app/src/main/java/com/example/scriptumapp/Login.java
@@ -1,6 +1,10 @@
 package com.example.scriptumapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
 
+    Button loginButton;
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,31 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loginButton = findViewById(R.id.loginButton);
+        signUp = findViewById(R.id.signUp);
+        loginButton.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.loginButton) {
+            //login en Firebase
+
+            // Hacer un intent para pasar a la MainActivity
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.signUp) {
+            //signup en firebase
+
+            // Hacer un intent para pasar a la MainActivity
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
