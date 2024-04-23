04c4555c24a28f5311730dd4adb91f626934088f
meveel25 <medinavelillaelvira@gmail.com>
2024-04-23 20:27:52 +0200

    variables e iniziali

--- app/src/main/java/com/example/scriptumapp/Queries.java
+++ app/src/main/java/com/example/scriptumapp/Queries.java
@@ -1,6 +1,8 @@
 package com.example.scriptumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Queries extends AppCompatActivity {
+public class Queries extends AppCompatActivity implements View.OnClickListener
+{
 
+    Button book_loan, book_exchange, gift_book;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,23 @@ public class Queries extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Inicializamos variables
+        book_loan = findViewById(R.id.book_loan);
+        book_exchange = findViewById(R.id.book_exchange);
+        gift_book = findViewById(R.id.gift_book);
+
+        //Ponemos a la escucha
+        book_loan.setOnClickListener(this);
+        book_exchange.setOnClickListener(this);
+        gift_book.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
     }
 }
