0645b5251bfc057c5633c55be62da000a2854fb7
meveel25 <medinavelillaelvira@gmail.com>
2024-04-23 20:02:52 +0200

    cambios en el codigo

--- app/src/main/java/com/example/scriptumapp/BookLoan.java
+++ app/src/main/java/com/example/scriptumapp/BookLoan.java
@@ -1,6 +1,8 @@
 package com.example.scriptumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BookLoan extends AppCompatActivity {
+public class BookLoan extends AppCompatActivity implements View.OnClickListener {
 
+    //Variables
+    Button backBookLoan, addBookLoan, editBookLoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,23 @@ public class BookLoan extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Inicializamos Variables
+        backBookLoan = findViewById(R.id.backBookLoan);
+        addBookLoan = findViewById(R.id.addBookLoan);
+        editBookLoan = findViewById(R.id.editBookLoan);
+
+        //Ponemos a la escucha
+        backBookLoan.setOnClickListener(this);
+        addBookLoan.setOnClickListener(this);
+        editBookLoan.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
     }
 }
