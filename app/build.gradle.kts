0f696b3b5657a70e752cda8efc3b0e7fc78edf38
maria.moreno <maria.moreno.r96@gmail.com>
2024-04-27 19:02:16 +0200

    "Arreglado signup"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,5 +42,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-firestore")
+    implementation ("com.google.android.gms:play-services-maps:20.0.0")
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 }
