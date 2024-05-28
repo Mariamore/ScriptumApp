bb0ce4150a5ad2c4a45e5217321e5c0fcb168dd8
meveel25 <medinavelillaelvira@gmail.com>
2024-05-28 04:15:25 +0200

    cambios gradle y fragment home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-
-
+    implementation("com.github.denzcoskum:ImagesSlideshow:0.1.2")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation ("com.github.smarteist:autoimageslider:1.4.0")
     implementation ("com.mikhaellopez:circularprogressbar:3.1.0")
--- app/src/main/java/com/example/scriptumapp/HomeFragment.java
+++ app/src/main/java/com/example/scriptumapp/HomeFragment.java
@@ -1,31 +1,20 @@
 package com.example.scriptumapp;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
     private ViewPager2 viewPager2;
-    private SliderAdapter adapter;
-    private List<SliderData> sliderDataList;
     private FirebaseStorage storage;
     private StorageReference storageRef;
 
@@ -42,45 +31,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        viewPager2 = rootView.findViewById(R.id.viewPager);
-        storage = FirebaseStorage.getInstance();
-        sliderDataList = new ArrayList<>();
-
-        storageRef = storage.getReference().child("booksData");
-
-        storageRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
-            @Override
-            public void onSuccess(ListResult listResult) {
-                if (listResult.getItems().isEmpty()) {
-                    Log.d(TAG, "No items found in the specified folder.");
-                }
-
-                for (StorageReference item : listResult.getItems()) {
-                    item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            sliderDataList.add(new SliderData(uri.toString()));
-
-                            if (sliderDataList.size() == listResult.getItems().size()) {
-                                adapter = new SliderAdapter(getContext(), sliderDataList);
-                                viewPager2.setAdapter(adapter);
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e(TAG, "Failed to get download URL", e);
-                        }
-                    });
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-                Log.e(TAG, "Failed to list items", e);
-            }
-        });
 
         return rootView;
     }
