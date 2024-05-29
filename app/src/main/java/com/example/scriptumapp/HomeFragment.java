d9fae95984124f84865656f908f183bef2cf98e1
maria.moreno <maria.moreno.r96@gmail.com>
2024-05-29 14:07:13 +0200

    añadido on click listener a los items del carrusel del homeFragment

--- app/src/main/java/com/example/scriptumapp/HomeFragment.java
+++ app/src/main/java/com/example/scriptumapp/HomeFragment.java
@@ -9,26 +9,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.widget.ViewPager2;
 import com.example.scriptumapp.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements ImageCarouselAdapter.OnItemClickListener {
 
     private FragmentHomeBinding binding;
     private List<List<String>> imageUrls = new ArrayList<>();
     private ImageCarouselAdapter adapter;
+    private FirebaseFirestore db;
+    private String bookId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        db = FirebaseFirestore.getInstance();
         return binding.getRoot();
     }
 
@@ -36,7 +44,7 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        adapter = new ImageCarouselAdapter(imageUrls);
+        adapter = new ImageCarouselAdapter(imageUrls, this);
         binding.viewPager.setAdapter(adapter);
         binding.viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
 
@@ -67,6 +75,44 @@ public class HomeFragment extends Fragment {
                         Log.e("Firestore", "Error getting documents.", task.getException());
                     }
                 });
+    }
+
+    @Override
+    public void onItemClick(String imageUrl) {
+            openDetailFragment(imageUrl);
+    }
+
+    private void openDetailFragment(String imageUrl) {
+        db.collection("booksData").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+
+                    for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                        if (document.contains("photo") && document.getString("photo").equals(imageUrl)) {
+                            bookId = document.getId();
+                        }
+
+                        }
+                    }
+                Log.d("bookid", bookId);
+                Bundle bundle = new Bundle();
+                bundle.putString("bookId", bookId);
+                BookInfoFragment fragment = new BookInfoFragment();
+                fragment.setArguments(bundle);
+
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_layout, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/scriptumapp/ImageCarouselAdapter.java
+++ app/src/main/java/com/example/scriptumapp/ImageCarouselAdapter.java
@@ -18,16 +18,22 @@ import java.util.List;
 public class ImageCarouselAdapter extends RecyclerView.Adapter<ImageCarouselAdapter.ImageViewHolder> {
 
     private List<List<String>> imageUrls;
+    private OnItemClickListener onItemClickListener;
 
-    public ImageCarouselAdapter(List<List<String>> imageUrls) {
+    public interface OnItemClickListener {
+        void onItemClick(String imageUrl);
+    }
+
+    public ImageCarouselAdapter(List<List<String>> imageUrls, OnItemClickListener onItemClickListener) {
         this.imageUrls = imageUrls;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_carousel_image, parent, false);
-        return new ImageViewHolder(view);
+        return new ImageViewHolder(view, onItemClickListener);
     }
 
     @Override
@@ -37,6 +43,24 @@ public class ImageCarouselAdapter extends RecyclerView.Adapter<ImageCarouselAdap
             Glide.with(holder.itemView.getContext()).load(imageUrlsGroup.get(0)).into(holder.imageView1);
             Glide.with(holder.itemView.getContext()).load(imageUrlsGroup.get(1)).into(holder.imageView2);
             Glide.with(holder.itemView.getContext()).load(imageUrlsGroup.get(2)).into(holder.imageView3);
+
+            holder.imageView1.setOnClickListener(v -> {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(imageUrlsGroup.get(0));
+                }
+            });
+
+            holder.imageView2.setOnClickListener(v -> {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(imageUrlsGroup.get(1));
+                }
+            });
+
+            holder.imageView3.setOnClickListener(v -> {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(imageUrlsGroup.get(2));
+                }
+            });
         }
     }
 
@@ -50,11 +74,13 @@ public class ImageCarouselAdapter extends RecyclerView.Adapter<ImageCarouselAdap
         ImageView imageView2;
         ImageView imageView3;
 
-        public ImageViewHolder(@NonNull View itemView) {
+        public ImageViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
             super(itemView);
             imageView1 = itemView.findViewById(R.id.imageView1);
             imageView2 = itemView.findViewById(R.id.imageView2);
             imageView3 = itemView.findViewById(R.id.imageView3);
+
+
         }
     }
 }
